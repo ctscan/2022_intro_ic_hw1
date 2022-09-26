63843d346d601374ef5ea2c11da9dbedce318876
Yuma-no-yuki <73100384+Yuma-no-yuki@users.noreply.github.com>
2022-09-26 14:21:56 +0800

    Create Q2.v

---
+++ 410903628/Q2.v
@@ -0,0 +1,13 @@
+module Q2(d,c,s,address);
+  input wire [15:0] d;
+  input wire c;
+  input wire s;
+  output reg address;
+  always @(*) begin
+        if(c == 0) address = d;      //c==0 && s==0 || c==0 && s==1    
+        else if(c == 1 && s == 0) address = d + 1;
+        else if(c == 1 && s == 1) address = d - 1;
+        else;
+    end
+    
+endmodule
