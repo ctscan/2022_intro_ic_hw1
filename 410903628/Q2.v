5096889929b7640f06e45b773c9cb9deb1596b81
Yuma-no-yuki <73100384+Yuma-no-yuki@users.noreply.github.com>
2022-09-26 14:31:03 +0800

    Update Q2.v

--- 410903628/Q2.v
+++ 410903628/Q2.v
@@ -4,10 +4,16 @@ module Q2(d,c,s,address);
   input wire s;
   output reg address;
   always @(*) begin
-        if(c == 0) address = d;      //c==0 && s==0 || c==0 && s==1    
-        else if(c == 1 && s == 0) address = d + 1;
-        else if(c == 1 && s == 1) address = d - 1;
-        else;
+    if(c == 0) begin 
+      address = d;      //c==0 && s==0 || c==0 && s==1
+    end
+    else if(c == 1 && s == 0) begin
+      address = d + 1;
+    end
+    else if(c == 1 && s == 1) begin
+      address = d - 1;
+    end
+    else;
   end
     
 endmodule
