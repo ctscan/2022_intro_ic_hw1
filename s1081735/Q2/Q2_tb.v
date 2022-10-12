6e64114aa1160e9bc0806cb2025cd998dc2dcc43
Colten <likeyou600@gmail.com>
2022-10-12 17:38:09 +0800

    Feat : 新增Q2 for迴圈測試 及Q2 Q3波形圖

--- s1081735/Q2/Q2_tb.v
+++ s1081735/Q2/Q2_tb.v
@@ -4,7 +4,7 @@ reg [15:0] d;
 reg c,s;
 
 wire [15:0] address;
-
+integer  i=0;
 Q2 q2(.d(d),.c(c),.s(s),.address(address));
 
 initial
@@ -12,94 +12,56 @@ begin
     $dumpfile("Q2.vcd");
     $dumpvars;
 
-    #0
-     d=16'h0000;
-    c=0;
-    s=0;
-    #1
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
+    for (i=0; i < 65536; i= i+1)
+    begin
+        d=16'h0000+i;
+        c=0;
+        s=0;
+        #1
+         if(d!=address)
+         begin
+             $display("output address error,when c=%d ,s=%d ",c,s);
+         end
+     end
 
-    #2
-     d=16'h0000;
-    c=0;
-    s=1;
-    #3
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
+     for (i=0; i < 65536; i= i+1)
+     begin
+         d=16'h0000+i;
+         c=0;
+         s=1;
+         #1
+          if(d!=address)
+          begin
+              $display("output address error,when c=%d ,s=%d ",c,s);
+          end
+      end
 
-    #4
-     d=16'h0000;
-    c=1;
-    s=0;
-    #5
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
+      for (i=0; i < 65536; i= i+1)
+      begin
+          d=16'h0000+i;
+          c=1;
+          s=0;
+          #1
+           if(d+1'b1!=address)
+           begin
+               $display("output address error,when c=%d ,s=%d ",c,s);
+           end
+       end
 
-    #6
-     d=16'h0000;
-    c=1;
-    s=1;
-    #7
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-
-    #8
-     d=16'hffff;
-    c=0;
-    s=0;
-    #9
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #10
-     d=16'hffff;
-    c=0;
-    s=1;
-    #11
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #12
-     d=16'hffff;
-    c=1;
-    s=0;
-    #13
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #14
-     d=16'hffff;
-    c=1;
-    s=1;
-    #15
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #16
-     d=16'h589a;
-    c=0;
-    s=0;
-    #17
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #18
-     d=16'h71f3;
-    c=0;
-    s=1;
-    #19
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #20
-     d=16'hc1ba;
-    c=1;
-    s=0;
-    #21
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
-
-    #22
-     d=16'h4ba3;
-    c=1;
-    s=1;
-    #23
-     $display("d:%h , c:%b , s:%b , address = %h", d,c,s,address);
+       for (i=0; i < 65536; i= i+1)
+       begin
+           d=16'h0000+i;
+           c=1;
+           s=1;
+           #1
+            if(d-1'b1!=address)
+            begin
+                $display("output address error,when c=%d ,s=%d ",c,s);
+            end
+        end
 
 
-
-    $finish;
+        $finish;
 end
 
 endmodule
