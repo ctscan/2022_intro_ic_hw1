3ef32b269623b8e02ed1928e4d9aecb4bc72384c
Yuma-no-yuki <73100384+Yuma-no-yuki@users.noreply.github.com>
2022-10-22 10:29:27 +0800

    Create Q2_tv.v

---
+++ s1090362/Q2_tv.v
@@ -0,0 +1,35 @@
+module Q2_tb();
+reg [15:0] d;
+reg c;
+reg s;
+
+wire [15:0] address;
+
+hw1 Q2(
+    .d(d),
+    .c(c),
+    .s(s),
+    .address(address)
+);
+
+initial begin
+    d = 5;
+    c = 0;
+    s = 0;
+    #20;
+    
+    d = 5;
+    c = 0;
+    s = 1;
+    #20;
+    
+    d = 5;
+    c = 1;
+    s = 0;
+    #20;
+    
+    d = 5;
+    c = 1;
+    s = 1;
+end
+endmodule
