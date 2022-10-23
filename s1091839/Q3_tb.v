6acf2be37fca93dac2be12c15f069b097b0c8a9d
s1091839 <114430008+s1091839@users.noreply.github.com>
2022-10-23 23:18:49 +0800

    Update Q3_tb.v

--- s1091839/Q3_tb.v
+++ s1091839/Q3_tb.v
@@ -3,12 +3,11 @@ module Q3_tb();
     wire O;
 
   Q3(.I(I),.C(C),.O(O));
-
     initial begin
         I=0;
         C=0;
         #10;
-
+        
         #10 I = 1;
         #10 I = 0;
         #10 I = 1;
@@ -18,7 +17,6 @@ module Q3_tb();
         #10 I = 1;
         #10 I = 0;
         #10 I = 1;
-
 
     end
 
