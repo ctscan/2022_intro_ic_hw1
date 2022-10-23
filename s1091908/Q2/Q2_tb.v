63a67d81972786ba904c8c5e54e22cef5c22a7ee
LianHsiang-Chen <113426889+LianHsiang-Chen@users.noreply.github.com>
2022-10-23 15:26:40 +0800

    Create Q2_tb.v

---
+++ s1091908/Q2/Q2_tb.v
@@ -0,0 +1,37 @@
+module Q2_tb();
+reg [15:0] d;
+reg c;
+reg s;
+
+wire [15:0] address;
+
+hw1 Q2(
+  .d(d)
+  .s(s)
+  .c(c)
+  .address(address)
+);
+  
+initial begin
+  d = 5;
+  c = 0;
+  s = 0;
+  #10;
+  
+  d = 5;
+  c = 0;
+  s = 1;
+  #10;
+  
+  d = 5;
+  c = 1;
+  s = 0;
+  #10;
+  
+  d = 5;
+  c = 1;
+  s = 1;
+end
+endmodule
+
+  
