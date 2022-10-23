eaa06edb3ae8f591fcbc7029ffa990b3c7a09d1b
shiang <89191153+Shiang0724@users.noreply.github.com>
2022-10-23 18:03:50 +0800

    Update Q2.v

--- s1090356/Q2.v
+++ s1090356/Q2.v
@@ -4,13 +4,13 @@ module Q2(d,c,s,address);
     output reg [15:0] address;
     always@(c or s) 
     begin
-        if(c == 1 && s == 0)begin
-         address = d + 1;
+        if(c==1&&s==0)begin
+         address=d+1;
         end
-        else if(c == 1 && s == 1)begin
-            address = d - 1;
+        else if(c==1&&s==1)begin
+            address=d-1;
         end
         else
-         address = d;
+         address=d;
     end
 endmodule
