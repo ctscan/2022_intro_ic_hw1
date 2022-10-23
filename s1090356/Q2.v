ed64b0d634abe843761facda0d83e549342e4d3e
shiang <89191153+Shiang0724@users.noreply.github.com>
2022-10-23 18:02:37 +0800

    Update Q2.v

--- s1090356/Q2.v
+++ s1090356/Q2.v
@@ -1,21 +1,16 @@
 module Q2(d,c,s,address);
-  input wire [15:0] d;
-  input wire c;
-  input wire s;
-  output reg address;
-  always @(*) begin
-    if(c == 0) begin 
-      address = d;      //c==0 && s==0 || c==0 && s==1
+    input [15:0] d;
+    input c,s;
+    output reg [15:0] address;
+    always@(c or s) 
+    begin
+        if(c == 1 && s == 0)begin
+         address = d + 1;
+        end
+        else if(c == 1 && s == 1)begin
+            address = d - 1;
+        end
+        else
+         address = d;
     end
-    else if(c == 1 && s == 0) begin
-      address = d + 1;
-    end
-    else if(c == 1 && s == 1) begin
-      address = d - 1;
-    end
-    else begin
-      address = d;
-    end
-  end
-    
 endmodule
