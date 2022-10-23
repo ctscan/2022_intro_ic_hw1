71d3ce1d403b6dc21dfc20117e4aa464de62fafd
sggito <113626079+sggito@users.noreply.github.com>
2022-10-23 19:29:40 +0800

    Update Q3_tb.v

--- s1090368/Q3/Q3_tb.v
+++ s1090368/Q3/Q3_tb.v
@@ -1,40 +1,36 @@
 module Q3_tb();
-  reg inp,clock;
-  wire out;
-  Q3 Q3_test(
-	  .inp(inp),
-    .clock(clock),
-    .out(out)
-  );	
+	reg inp,clock;
+	wire out;
+	Q3 Q3_test(
+		.inp(inp),
+    		.clock(clock),
+    		.out(out)
+  	);	
 	always #5 clock=~clock;
 	
-  initial begin     
-    $dumpfile("Q3.vcd");
-    $dumpvars;
-
-    inp=1;
+  	initial begin     
+    		inp=1;
 		clock=0;
 
-    #10 
+    		#10 
 		inp=0;
-    #10 
+    		#10 
 		inp=1;
-    #10 
+    		#10 
 		inp=0;
-    #10 
+    		#10 
 		inp=1;
-    #10
+    		#10
 		inp=0;
 		#10 
 		inp=1;
-    #10
+    		#10
 		inp=0;
 		#10 
 		inp=1;
-    #10
+    		#10
 		inp=0;
-    #10 		
+    		#10 		
 		$finish;
-    end
-
+    	end
 endmodule
