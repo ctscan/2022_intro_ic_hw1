399822273b266173c44ad27dac7ca23b14a108f0
sggito <113626079+sggito@users.noreply.github.com>
2022-10-23 19:24:52 +0800

    Create Q3.v

---
+++ s1090368/Q3/Q3.v
@@ -0,0 +1,11 @@
+module Q3(inp,out,clock);
+  input inp,clock;
+  output reg out=0;
+	reg[7:0] temp=8'b0;
+  always @(posedge clock)begin
+      if(inp==1) temp = temp+temp+1;
+		  else if(inp==0)temp = temp<<1;
+		  if(temp%5==0) out = 1;
+		  else out = 0;
+  end
+endmodule
