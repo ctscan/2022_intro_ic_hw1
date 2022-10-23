1439f31f9ff5904949d8b242c4ff9eee2854c9ac
Volleyball2022 <100548823+Volleyball2022@users.noreply.github.com>
2022-10-23 22:17:01 +0800

    Update Answer.v

--- 410817762/Q3/Answer.v
+++ 410817762/Q3/Answer.v
@@ -3,12 +3,28 @@ module main(
     input clock,
     output reg out
 );
+    parameter n0 = 3'b000 ,n1 = 3'b001 ,n2 = 3'b010 ,n3 = 3'b011 ,n4 = 3'b100 ;
+    reg[2:0] state;
+
     initial begin
-        assign tamp1=inp%5;
         state=n0;
         out=0;
     end
+
     always @(posedge clock)begin
-        out=tamp1==0;
+        case(state)
+            n0: 
+                state=inp==0 ? n0:n1;
+            n1: 
+                state=inp==0 ? n2:n3;
+            n2: 
+                state=inp==0 ? n4:n0;
+            n3: 
+                state=inp==0 ? n1:n2;
+            n4: 
+                state=inp==0 ? n3:n4;
+        endcase
+
+        out=state==n0;
     end
 endmodule
