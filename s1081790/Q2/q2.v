b9e0e0bfa6f2c47b0f3cbf98925f9b2fd185bf17
xinnnyuuuu <105032460+xinnnyuuuu@users.noreply.github.com>
2022-10-23 14:40:07 +0800

    Update q2.v

--- s1081790/Q2/q2.v
+++ s1081790/Q2/q2.v
@@ -1,14 +1,13 @@
-module Q2 (address, d, c, s);
+module Q2 (c, s, d, address);
 
-	output reg [15:0] address;
 	input  c, s;	
 	input  [15:0]  d;
+	output reg [15:0] address;
 
 	always @(*) begin
 		if(c == 0) address = d;
 		else if(c == 1 && s == 1) address = d - 1;
 		else if(c == 1 && s == 0) address = d + 1;
-		else;
 	end
 
 endmodule
