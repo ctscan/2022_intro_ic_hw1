bd3cc00d31867f2330a6dcd6cc9ee8e68d3374b8
shiang <89191153+Shiang0724@users.noreply.github.com>
2022-10-23 18:10:29 +0800

    Update Q2.v

--- s1090356/Q2.v
+++ s1090356/Q2.v
@@ -1,16 +1,22 @@
 module Q2(d,c,s,address);
-    input [15:0] d;
-    input c,s;
+    input wire [15:0] d;
+    input wire c;
+    input wire s;
     output reg [15:0] address;
-    always@(c or s) 
+    always @(*) begin
     begin
-        if(c==1&&s==0)begin
-         address=d+1;
+        if(c==0)begin
+            address=d;
+        end
+        else if(c==1&&s==0)begin
+            address=d+1;
         end
         else if(c==1&&s==1)begin
             address=d-1;
         end
-        else
-         address=d;
+        else begin
+            address=d;
+        end
     end
+        
 endmodule
