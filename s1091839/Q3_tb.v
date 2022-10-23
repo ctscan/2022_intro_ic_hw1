9df128eaec4dd73e922f6a672fb32308d8a0d60a
s1091839 <114430008+s1091839@users.noreply.github.com>
2022-10-23 23:18:18 +0800

    Create Q3_tb.v

---
+++ s1091839/Q3_tb.v
@@ -0,0 +1,25 @@
+module Q3_tb();
+    reg I,C;
+    wire O;
+
+  Q3(.I(I),.C(C),.O(O));
+
+    initial begin
+        I=0;
+        C=0;
+        #10;
+
+        #10 I = 1;
+        #10 I = 0;
+        #10 I = 1;
+        #10 I = 0;
+        #10 I = 1;
+        #10 I = 0;
+        #10 I = 1;
+        #10 I = 0;
+        #10 I = 1;
+
+
+    end
+
+endmodule
