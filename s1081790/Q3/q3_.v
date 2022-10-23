cb729cfae0649e950e291c4306d5ba564d5e4605
xinnnyuuuu <105032460+xinnnyuuuu@users.noreply.github.com>
2022-10-23 14:11:38 +0800

    Add files via upload

---
+++ s1081790/Q3/q3_.v
@@ -0,0 +1,31 @@
+`include "q3.v"
+`timescale 1ns/1ns
+
+ module q3_();
+    reg i,c;
+    wire out;
+
+    q3 test(i,c,out);
+    
+    always #10 c = ~c;
+
+    initial begin
+      $dumpfile("q3.vcd");
+      $dumpvars;
+
+      i=0;
+      c=0;
+         
+         #10 i=1;
+         #10 i=0;
+         #10 i=1;
+         #10 i=0;
+         #10 i=1;
+         #10 i=0;
+         #10 i=1;
+         
+         #100 $finish;
+         
+    end
+
+ endmodule
